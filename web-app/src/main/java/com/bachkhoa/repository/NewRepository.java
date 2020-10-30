f9f37a91101df6a0e52302976071a0bd9c01ecf5
nguyenhao <nguyenhao.mailfortest@gmail.com>
2020-10-30 11:23:01 +0700

    add JPA connect

---
+++ web-app/src/main/java/com/bachkhoa/repository/NewRepository.java
@@ -0,0 +1,9 @@
+package com.bachkhoa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bachkhoa.entity.NewEntity;
+
+public interface NewRepository extends JpaRepository<NewEntity, Long>{
+
+}
