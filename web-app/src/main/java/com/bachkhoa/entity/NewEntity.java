de793e9d8bf5835a37996712137c5f43ba39f3fb
nguyenhao <nguyenhao.mailfortest@gmail.com>
2020-11-13 17:20:06 +0700

    add spring restful web service

--- web-app/src/main/java/com/bachkhoa/entity/NewEntity.java
+++ web-app/src/main/java/com/bachkhoa/entity/NewEntity.java
@@ -2,9 +2,9 @@ package com.bachkhoa.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +22,10 @@ public class NewEntity extends BaseEntity {
 
 	@Column(name = "content", columnDefinition = "TEXT")
 	private String content;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id")
+	private CategoryEntity category;
 
 	public String getTitle() {
 		return title;
@@ -53,5 +57,13 @@ public class NewEntity extends BaseEntity {
 
 	public void setContent(String content) {
 		this.content = content;
+	}
+
+	public CategoryEntity getCategory() {
+		return category;
+	}
+
+	public void setCategory(CategoryEntity category) {
+		this.category = category;
 	}
 }
